59611f229d28334725957928aca33cc0608f5aa0
Kab <kabalan.gaspard@looker.com>
2019-10-02 14:32:36 +0100

    feat (kabalangaspard): add setPort function

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -63,6 +63,10 @@
     this.basePath = this._basePath.replace('INSTANCE_NAME',name);
   }
 
+  exports.prototype.setPort = function(port) {
+    this.basePath = this._basePath.replace('19999',port);
+  }
+
   /**
    * Returns a string representation for an actual parameter.
    * @param param The actual parameter.
